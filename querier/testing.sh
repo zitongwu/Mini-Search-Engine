1020470b917ca888575736d3e5aff716b8377d6c
Jolly <71951467+zitongwu0301@users.noreply.github.com>
2021-04-03 12:00:36 +0800

    Update testing.sh

--- querier/testing.sh
+++ querier/testing.sh
@@ -1,4 +1,4 @@
-# testing.sh - test for the querier module
+## testing.sh - test for the querier module
 # Zitong Wu, Feburary 22, 2021
 #!/bin/bash
 
@@ -9,30 +9,30 @@
 ./querier
 
 # one argument
-./querier data/depth2 
+./querier ../crawler/data/depth2 
 
 # more than two arguments
-./querier data/depth2 indexFile 3
+./querier ../crawler/data/depth2 ../indexer/indexFile 3
 
 
 ######## invalid first argument
 # first argument pageDirectory does not exist 
-./querier foo/ indexFile
+./querier foo/ ../indexer/indexFile
 
 # first argument pageDirectory is not a Crawler-produced directory
 mkdir dir
-./querier dir/ indexFile
+./querier dir/ ../indexer/indexFile
 rm -rf dir
 
 
 ######## invalid second argument
 # second argument indexFileName does not exist
-./querier data/depth1 indexFile2
+./querier ../crawler/data/depth1 indexFile2
 
 # second argument indexFileName exists but is not readable
 touch indexFile2
 chmod -r ./indexFile2
-./querier data/depth1 indexFile2
+./querier ../crawler/data/depth1 indexFile2
 rm -f indexFile2
 
 
@@ -40,13 +40,13 @@ rm -f indexFile2
 ####### incorrect queries 
 # test queries containing special characters and having operators at incorrect positions
 # use self-created incorrect query examples
-./querier data/depth2 indexFile < incorrectQueries
+./querier ../crawler/data/depth2 ../indexer/indexFile < incorrectQueries
 
 
 ####### correct queries
 # test different combinations of "and", "or" operators 
 # use self-created correct query examples
-./querier data/depth2 indexFile < correctQueries
+./querier ../crawler/data/depth2 ../indexer/indexFile < correctQueries
 
 # use fuzzquery
-./fuzzquery indexFile 10 0 | ./querier data/depth2 indexFile
+./fuzzquery ../indexer/indexFile 10 0 | ./querier ../crawler/data/depth2 ../indexer/indexFile
